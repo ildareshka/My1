d9758930baddea87e4902954889e54a2d5a7253f
ildareshka <ildareshka@mail.ru>
2015-02-20 12:09:44 +0300

    меж акт

--- app/src/main/java/ru/home/root/my1/MainActivity.java
+++ app/src/main/java/ru/home/root/my1/MainActivity.java
@@ -5,18 +5,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
     private TextView txtView;
     private Button btn2;
+    private EditText txt1;
+    private EditText txt2;
+    private EditText txt3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
 
         txtView=(TextView)findViewById(R.id.textView);
+        txt1=(EditText)findViewById(R.id.editText);
+        //txt2=(EditText)findViewById(R.id.editText2);
+        //txt2=(EditText)findViewById(R.id.editText3);
+
         btn2=(Button)findViewById(R.id.button2);
         btn2.setOnClickListener(new View.OnClickListener()  {
             @Override
@@ -32,6 +40,10 @@ public class MainActivity extends Activity {
     public void goAct(View view)
     {
         Intent int1=new Intent(this,layout1.class);
+        int1.putExtra("editText1",txt1.getText().toString());
+        //int1.putExtra("editText2",txt2.getText());
+        //int1.putExtra("editText3",txt3.getText());
+
         startActivity(int1);
     }
 }
--- app/src/main/java/ru/home/root/my1/layout1.java
+++ app/src/main/java/ru/home/root/my1/layout1.java
@@ -3,12 +3,20 @@ package ru.home.root.my1;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class layout1 extends Activity {
+    private TextView txt1;
+    private TextView txt2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout1);
+
+        txt1=(TextView)findViewById(R.id.textView2);
+        txt2=(TextView)findViewById(R.id.textView3);
+        txt1.setText(getIntent().getStringExtra("editText1"));
+        //txt2.setText(getIntent().getStringExtra("editText2")+"-"+getIntent().getStringExtra("editText3"));
     }
     public void btnBack(View view){
         finish();
